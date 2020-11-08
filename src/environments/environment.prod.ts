97cf0f07a4861da24e24296c59bf8249773fb7b6
emmadal <emmanueldalougou@gmail.com>
2020-11-08 18:51:47 +0000

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+    firebase: {
+    apiKey: "AIzaSyD1iHBMskR3zuuKGLQjB9ecbBURjOShUPc",
+    authDomain: "foodapp-93846.firebaseapp.com",
+    databaseURL: "https://foodapp-93846.firebaseio.com",
+    projectId: "foodapp-93846",
+    storageBucket: "foodapp-93846.appspot.com",
+    messagingSenderId: "135605586486",
+    appId: "1:135605586486:web:a21275a807b2694cb8a697",
+    measurementId: "G-09BGGV0JJP",
+  },
+  onesignal: {
+    appId: "128d1749-e049-4c8f-97ca-59e99cae8f41",
+    googleProjectNumber: "135605586486",
+    restKey: "MGUzMzVkNmQtYzc4Ny00YzI3LTk1ZTMtMTUxNWQ2OTFmNmU3",
+  },
+  general: {
+    symbol: '$',
+    code: 'USD'
+  }
+};
